ec154b67b51b7b3e2e11dc5410e556c568326149
Yuliia <julya97123@gmail.com>
2018-11-04 17:30:30 +0200

    added columnar transposition algorithm [work in progress]

--- src/ColumnarTransposition.java
+++ src/ColumnarTransposition.java
@@ -1,8 +1,32 @@
+import java.util.Arrays;
+
 public final class ColumnarTransposition {
-    public static String encrypt(String message, int key){
-        char[] result = message.toCharArray();
+    public static String encrypt(String message,String  key){
+        int rows = (int) Math.ceil(message.length()/key.length());
+        int columns = key.length();
+        char [][] table = new char[rows][columns];
+
+        for (int i = 0, k=0; i < columns; i++) {
+            for (int j = 0; j <rows ; j++, k++) {
+                table[j][i] = message.charAt(k);
+            }
+        }
+
     }
     public static String decrypt(String message, int key){
 
     }
+
+    private int[] calculateColumnOrder(String key){
+        int[] order = new int[key.length()];
+        char [] chars =key.toCharArray();
+        char[] sortedChars = key.toCharArray();
+        Arrays.sort(sortedChars);
+        for (int i = 0; i <order.length; i++) {
+            for (int j = 0; j <sortedChars.length ; j++) {
+                if (sortedChars[j] == )
+            }
+        }
+    }
 }
+
